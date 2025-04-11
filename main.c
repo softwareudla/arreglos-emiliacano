57821615ad8a8b95e1613d7b35b26508b6fe2873
emilia.cano <emilia.cano@udla.edu.ec>
2025-04-10 19:28:22 -0500

    inicio

--- main.c
+++ main.c
@@ -15,8 +15,7 @@ int main() {
     int aprobados[NUM_ASIGNATURAS] = {0}, reprobados[NUM_ASIGNATURAS] = {0};
     char nombres[NUM_ESTUDIANTES][MAX_NOMBRE];
     char asignaturas[NUM_ASIGNATURAS][MAX_NOMBRE];
- 
- 
+
     for (int j = 0; j < NUM_ASIGNATURAS; j++) {
         int valido = 0;
         while (!valido) {
@@ -26,48 +25,53 @@ int main() {
 
             valido = 1;
             for (int i = 0; asignaturas[j][i] != '\0'; i++) {
-                if (asignaturas[j][i] >= '0' || asignaturas[j][i] <= '9') {
-                    valido = 0;
-                    break;
+                if (asignaturas[j][i] >= '0') {
+                    if (asignaturas[j][i] <= '9') {
+                        valido = 0;
+                        break;
+                    }
                 }
             }
 
-            if (!valido || (asignaturas[j]) == 0) {
-                printf("Error. No se permiten números. Intente de nuevo.\n");
+            if (!valido || asignaturas[j][0] == '\0') {
+                printf("Error. No se permiten números o campos vacíos. Intente de nuevo.\n");
             }
         }
     }
- 
+
     for (int i = 0; i < NUM_ASIGNATURAS; i++) {
         maxNotaMateria[i] = 0;
         minNotaMateria[i] = 10;
     }
+
     for (int i = 0; i < NUM_ESTUDIANTES; i++) {
         int valido = 0;
         while (!valido) {
-    
             printf("Ingrese el nombre del estudiante %d: ", i + 1);
             fgets(nombres[i], MAX_NOMBRE, stdin);
             nombres[i][strcspn(nombres[i], "\n")] = '\0';
 
             valido = 1;
             for (int k = 0; nombres[i][k] != '\0'; k++) {
-                if (nombres[i][k] >= '0' || nombres[i][k] <= '9') {
-                    valido = 0;
-                    break;
+                if (nombres[i][k] >= '0') {
+                    if (nombres[i][k] <= '9') {
+                        valido = 0;
+                        break;
+                    }
                 }
             }
 
-            if (!valido || (nombres[i]) == 0) {
-                printf("Error. No se permiten números. Intente de nuevo.\n");
+            if (!valido || nombres[i][0] == '\0') {
+                printf("Error. No se permiten números o campos vacíos. Intente de nuevo.\n");
             }
         }
+
         printf("Estudiante: %s\n", nombres[i]);
         for (int j = 0; j < NUM_ASIGNATURAS; j++) {
             do {
                 printf("Ingrese la nota para %s (0-10): ", asignaturas[j]);
                 int result = scanf("%f", &notas[i][j]);
-                while (getchar() != '\n'); 
+                while (getchar() != '\n');
 
                 if (result != 1 || notas[i][j] < 0 || notas[i][j] > 10) {
                     printf("Error. Dato no permitido.\n");
@@ -86,7 +90,7 @@ int main() {
             if (notas[i][j] < minNotaMateria[j]) minNotaMateria[j] = notas[i][j];
         }
     }
-    
+
     printf("\n\n--- Resultados por Estudiante  ----\n\n");
     for (int i = 0; i < NUM_ESTUDIANTES; i++) {
         sumaEstudiante = 0;
@@ -100,18 +104,17 @@ int main() {
         }
 
         promedioEstudiante[i] = sumaEstudiante / NUM_ASIGNATURAS;
-      
         printf("Estudiante: %s \nPromedio: %.2f \nNota más alta: %.2f \nNota más baja: %.2f\n", 
-                nombres[i], promedioEstudiante[i], maxNotaEstudiante, minNotaEstudiante);
+               nombres[i], promedioEstudiante[i], maxNotaEstudiante, minNotaEstudiante);
     }
-   
+
     printf("\n\n--- Resultados por Materia --\n\n");
     for (int j = 0; j < NUM_ASIGNATURAS; j++) {
         promedioMateria[j] = sumaMateria[j] / NUM_ESTUDIANTES;
-      
-        printf("Asignatura: %s \nPromedio: %.2f \nNota más alta: %.2f \nNota más baja: %.2f", 
-                asignaturas[j], promedioMateria[j], maxNotaMateria[j], minNotaMateria[j]);
-        printf("\nEstudiantes aprobados: %d \nEstudiantes reprobados: %d\n", aprobados[j], reprobados[j]);
+        printf("Asignatura: %s \nPromedio: %.2f \nNota más alta: %.2f \nNota más baja: %.2f\n", 
+               asignaturas[j], promedioMateria[j], maxNotaMateria[j], minNotaMateria[j]);
+        printf("Estudiantes aprobados: %d \nEstudiantes reprobados: %d\n", aprobados[j], reprobados[j]);
     }
+
     return 0;
 }
